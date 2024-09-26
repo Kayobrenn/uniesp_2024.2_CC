5d5f7da5d21eab9caa118378d2e0a4a391487108
Kayo Brenno <Kbreno822@gmail.com>
2024-09-25 21:22:54 -0300

    Atualização da Questão

--- Aula IP - Messias/Questão_12.py
+++ Aula IP - Messias/Questão_12.py
@@ -1,11 +1,31 @@
 def validar_arma(ataque, durabilidade, nome_arma):
+    
     if ataque > 50 and durabilidade > 70:
-        print('Arma Validada!')
+        print('Arma validada!')
         return nome_arma
-    else: 
-        return 'erro'
+    else:
+        return 'error'
 
-if __name__ == '__main__'
+
+if __name__ == '__main__':
+
+    controle_opcao = 1000
+
+    while controle_opcao != -1:
+        nome_arma = input('Digite o nome da arma: ')
+        ataque_arma = int(input('Digite o ataque da arma: '))
+        durabilidade_arma = int(input('Digite a durabilidade da arma: '))
+
+        resultado_validacao = validar_arma(ataque_arma,
+                                           durabilidade_arma,
+                                           nome_arma)
+        
+        if resultado_validacao != 'error':
+            print(f'A sua arma {resultado_validacao}, foi validada!!!')
+        else:
+            print('Essa arma não funciona!')
+
+        controle_opcao = int(input('Digite: \n0 para continuar e \n-1 para finalizar'))
 
 espada_ataque = int(input('Digite o ataque da espada: '))
 espada_durabilidade = int(input('Digite a durabilidade da espada: '))
