0f665b42cc5ef70306cf79f652e5fd6fa282daea
Kayo Brenno <Kbreno822@gmail.com>
2025-02-23 17:36:56 -0300

    Questão 1 JAVA

---
+++ Questões JAVA/Main1.java
@@ -0,0 +1,10 @@
+public class Main1 {
+    public static void main(String[] args) {
+        double valorhora = 15.00;
+        int quantidadehoras = 40;
+
+        double salario = valorhora * quantidadehoras;
+
+        System.out.println("O salario do funcionario é " + salario + " Reais");
+    }
+}
